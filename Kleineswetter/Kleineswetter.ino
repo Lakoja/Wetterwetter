a5f586e85ce6d48f9c9c3fda4457596ac6ab690e
Ulrich Kuhn <me@you.com>
2017-08-22 22:40:40 +0200

    Add missing license notice

--- Kleineswetter/Kleineswetter.ino
+++ Kleineswetter/Kleineswetter.ino
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Lakoja on github.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <Arduino.h>
 #include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
@@ -5,7 +21,6 @@
 Adafruit_BME280 bme;
 bool bme_ok = false;
 
-// TODO center in server awake window
 // TODO save last temp and only connect when necessary
 
 void setup() {
